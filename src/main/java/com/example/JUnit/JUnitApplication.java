a843989de0693bd280c6734701a2a3d2bc49e7fd
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2022-09-26 00:41:56 +0530

    pr-init

---
+++ src/main/java/com/example/JUnit/JUnitApplication.java
@@ -0,0 +1,13 @@
+package com.example.JUnit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JUnitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JUnitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JUnit/JUnitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JUnit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JUnitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
